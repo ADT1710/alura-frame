fb59409bfadf672fd8131f5c0644d1753cffb7f6
adt1710 <adrianoibira.adt@gmail.com>
2022-12-13 06:41:09 -0300

    Utilizando arrow function na função de formatação de data.

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -9,8 +9,10 @@ class NegociacaoController{
     adiciona(event){
         event.preventDefault();
 
-        let data = new Date(...this._inputData.value.split("-").map(function (item,indice) {return item - indice % 2; }))
+        let data = new Date(...this._inputData.value.split("-").map((item,indice) => item - indice % 2));
         console.log(`${data} | ${typeof(data)}`)
+
+        let negociacao = new Negociacao(data, this._inputQuantidade.value, this._inputValor.value)
         // let negociacao = new Negociacao(
         //     this._inputData.value,
         //     this._inputQuantidade.value,
