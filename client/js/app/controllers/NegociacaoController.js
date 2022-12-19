a3ffcf70932c84dc7c5b1d42dc903eefce5e6328
adt1710 <adrianoibira.adt@gmail.com>
2022-12-19 08:30:26 -0300

    Removendo carga de formatação de data do Controller, pois foi atribuída para a DateHelper.

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -8,12 +8,13 @@ class NegociacaoController{
     }
     adiciona(event){
         event.preventDefault();
+        let helper = new DateHelper();
+        let data = helper.textoParaData(this._inputData.value);
 
-        let data = new Date(...this._inputData.value.split("-").map((item,indice) => item - indice % 2));
+        let negociacao = new Negociacao(data, this._inputQuantidade.value, this._inputValor.value);
+        console.log(negociacao);
 
-        let negociacao = new Negociacao(data, this._inputQuantidade.value, this._inputValor.value)
+        console.log(helper.dataParaTexto(negociacao.data))
 
-        let diaMesAno = `${negociacao.data.getDate()}/${(negociacao.data.getMonth()+1)}/${negociacao.data.getFullYear()}`;
-        console.log(diaMesAno);
     }
 }
